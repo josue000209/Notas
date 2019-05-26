9b589fb48bd12a0f78309b0e9f1d164e360f67e3
sarielpinto <48193176+sarielpinto@users.noreply.github.com>
2019-05-26 11:56:04 -0500

    parte3

--- app/src/main/java/com/example/notas/view/NotesAdapter.java
+++ app/src/main/java/com/example/notas/view/NotesAdapter.java
@@ -42,7 +42,7 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.note_list_row, parent, false);
+                .inflate(R.layout.note_list_row, parent, true);
 
         return new MyViewHolder(itemView);
     }
